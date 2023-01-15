46e347534d3c0b35e975edd8f1e22f0c6f0f6ee6
CuteFox8302 <111240985+CuteFox8302@users.noreply.github.com>
2023-01-15 16:43:49 -0500

    Initial commit.

---
+++ src/main/java/net/cutefox/afewmorefoods/AFewMoreFoodsClient.java
@@ -0,0 +1,10 @@
+package net.cutefox.afewmorefoods;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AFewMoreFoodsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
